de54d51b40577d69d3115dfa7a5cf2656e7bfdad
KasparasKralikas <almyka@gmail.com>
2018-10-06 18:12:16 +0300

    Initial Commit
    A basic game loop for Windows Forms

---
+++ KKParticleSystem/GameForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KKParticleSystem
+{
+    public partial class GameForm : Form
+    {
+        public GameForm()
+        {
+            InitializeComponent();
+            Application.Idle += HandleApplicationIdle;
+        }
+
+
+        void HandleApplicationIdle(object sender, EventArgs e)
+        {
+            while (IsApplicationIdle())
+            {
+                GameUpdate();
+                GameRender();
+            }
+        }
+
+        void GameUpdate()
+        {
+
+        }
+
+        void GameRender()
+        {
+
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool IsApplicationIdle()
+        {
+            NativeMessage result;
+            return PeekMessage(out result, IntPtr.Zero, (uint)0, (uint)0, (uint)0) == 0;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NativeMessage
+        {
+            public IntPtr Handle;
+            public uint Message;
+            public IntPtr WParameter;
+            public IntPtr LParameter;
+            public uint Time;
+            public Point Location;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int PeekMessage(out NativeMessage message, IntPtr window, uint filterMin, uint filterMax, uint remove);
+    }
+}
+
